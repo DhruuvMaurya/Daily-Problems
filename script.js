122bb0195ec74d45d1988df630c4447fc698d23b
Dhruv Maurya <dhruv958maurya@gmail.com>
2022-08-12 23:44:20 +0530

    Problem 2: Get the nth character in a string

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-// Get last n characters of a string
+// JAVASCRIPT BASICS
+
+// 1. Get last n characters of a string
 
 function myFunction(str) {
   return str.slice(-3);
+}
+
+// 2. Get the Nth Character in a String
+
+function myFunction(a, n) {
+  return a.charAt(n - 1);
+}
+
+function myFunction(a, n) {
+  return a[n - 1];
+}
+
+// 3. Get first 3 character of string
+
+function myFunction(a) {
+  return a.slice(3);
 }
